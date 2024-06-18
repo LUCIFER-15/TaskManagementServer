aab6dc7b5e15bd900bd607d9b890c687aa234b84
Suraj <surajdhote44@gmail.com>
2024-06-18 18:49:52 +0530

    all updated

--- dB.js
+++ dB.js
@@ -7,7 +7,12 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-app.use(cors());
+const corsOptions = {
+  origin: 'https://task-management-new-dusky.vercel.app', // Vercel frontend URL
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 
 const tasksFilePath = './tasks.json';
 
